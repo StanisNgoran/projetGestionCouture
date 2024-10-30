6479ed46448718c84888c8a19e65e8c7787022ec
stanis <ngoranwatimy@gmail.com>
2024-10-30 14:10:35 +0000

    Second modification

--- project/coutureApp/urls.py
+++ project/coutureApp/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('client/', views.creerEtAfficher_client, name='client'),  # Utilisation de la vue pour création/affichage
+    path('commande/', views.commande, name='commande'),
+    path('tenue/', views.tenue, name='tenue'),
+    path('image/', views.image, name='image'),
+    path('facture/', views.facture, name='facture'),
+    path('editefacture/', views.editefacture, name='editefacture'),
+    path('clientlist/', views.clientlist, name='clientlist'),
+    path('commandlist/', views.commandlist, name='commandlist'),
 ]
+
+
 
 
--- project/coutureApp/views.py
+++ project/coutureApp/views.py
@@ -1,8 +1,56 @@
 # coutureApp/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.models import Client
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'home.html')
 
 
+def client(request):
+    return render (request,'client.html')
+
+def commande(request):
+    return render (request,'commande.html')
 # Create your views here.
+
+def tenue(request):
+    return render (request,'tenue.html')
+
+def image(request):
+    return render(request,'image.html')
+
+def facture(request):
+    return render(request,'facture.html')
+
+def editefacture(request):
+    return render(request,'editefacture.html')
+
+def clientlist(request):
+    return render(request,'clientlist.html')
+
+
+def commandlist(request):
+    return render(request,'commandlist.html')
+
+
+   
+def creerEtAfficher_client(request):
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        prenom = request.POST.get('prenom')
+        contact = request.POST.get('contact')
+        
+        # Créer un nouvel objet Client et l'enregistrer dans la base de données
+        client = Client(nom=nom, prenom=prenom, contact=contact)
+        client.save()
+        messages.success(request, 'Client Enregistré avec Succès!')
+        # Rediriger vers la même page pour afficher le client nouvellement ajouté
+        return redirect('commande')  # Rediriger vers la même vue pour éviter les re-soumissions
+
+    # Récupère tous les clients pour les afficher dans le tableau
+    clients = Client.objects.all().order_by('-ajout')
+    clients = Client.objects.all()
+    return render(request, 'client.html', {'clients': clients})  # Utilisez 'clients' ici
+
